da306f79427d253724514604d4a2922109858d39
Wolfgang Hennerbichler <wogri@wogri.com>
2022-03-17 21:58:55 +0100

    openapi updates

--- golang/go/api.go
+++ golang/go/api.go
@@ -12,7 +12,6 @@ package openapi
 import (
 	"context"
 	"net/http"
-	"os"
 )
 
 
@@ -53,6 +52,6 @@ type DefaultApiServicer interface {
 	TemperaturePost(context.Context, NewTemperature) (ImplResponse, error)
 	UserPost(context.Context, User) (ImplResponse, error)
 	VarroaScanGet(context.Context, string, string, string, int64, string, int64, int64) (ImplResponse, error)
-	VarroaScanImagePost(context.Context, string, string, string, int64, *os.File) (ImplResponse, error)
+	VarroaScanImagePost(context.Context, string, string, string, int64, string) (ImplResponse, error)
 	VarroaScanPost(context.Context, int64, VarroaScan) (ImplResponse, error)
 }
